cb6c59a402af32cc0e93f0c62c10c9ec9254462e
tomoyuki-hiranuma <numanumatom@gmail.com>
2024-05-06 12:20:32 +0900

    add: iam_role

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
-module "web_server" {
-  source        = "./http_server"
-  instance_type = "t2.micro"
+# module "web_server" {
+#   source        = "./http_server"
+#   instance_type = "t2.micro"
+# }
+
+# output "public_dns" {
+#   value = module.web_server.public_dns
+# }
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect   = "Allow"
+    actions  = ["ec2:DescribeRegions"]
+    resources = ["*"]
+  }
 }
 
-output "public_dns" {
-  value = module.web_server.public_dns
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "ec2-describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
